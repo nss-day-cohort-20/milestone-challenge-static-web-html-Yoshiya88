650967b9f2fd4c688be900c87472d65ad782f84e
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-06-09 16:55:43 -0500

    start javascript

---
+++ main.js
@@ -0,0 +1,30 @@
+// The page will have a linked JavaScript file for the blog posts section. Create an array to hold your posts (at least 2 posts). Each item in the array must be an object that contains at a minimum the following information: Title, Copy, Date. Use JavaScript to add each post to the DOM.
+
+// var blog = [
+// {
+// 	name:
+// 	date:
+// 	text:
+// },
+// {
+// 	name:
+// 	date:
+// 	text:
+// }
+// ]
+
+// var blogPost = document.getElementsByClass("blog")
+// for (var i = 0; i < products.length; i++)
+// 	blogPost.innerHTML += "<section class='blog'>"
+
+
+
+
+
+
+
+
+
+
+
+
